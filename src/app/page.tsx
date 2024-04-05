bd403126c69fe6b33bc92e78f4f67f652c8f38f4
ajinkya singh <azinc28@gmail.com>
2024-04-05 22:56:39 +0530

    *dev mode* HomePage

---
+++ src/app/page.tsx
@@ -0,0 +1,77 @@
+import MaxWidthWrapper from "@/component/MaxWidthWrapper";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+export default function Home() {
+  return (
+    <><div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20 mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-normal space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-gray-200">
+            <p className="text-sm font-semibold text-gray-700">Asher is now public</p>
+        </div>
+        <h1 className='max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl'>
+              Chat with your{' '}
+              <span className='text-blue-600'>documents</span>{' '}
+              in seconds.
+        </h1>
+        <p className='mt-5 max-w-prose text-zinc-700 sm:text-lg'>
+          Quill allows you to have conversations with any
+          PDF document. Simply upload your file and start
+          asking questions right away.
+        </p>
+        <Link
+          className={buttonVariants({
+            size: 'lg',
+            className: 'mt-5',
+          })}
+          href='/dashboard'
+          target='_blank'>
+          Get started{' '}
+          <ArrowRight className='ml-2 h-5 w-5' />
+        </Link>
+    </div>
+    <div>
+      <div className="relative isolate">
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] md:opacity-30 opacity-40 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <Image
+                    src='/dashboard-preview.jpg'
+                    alt='product preview'
+                    width={1364}
+                    height={866}
+                    quality={100}
+                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 -bottom-20 sm:-bottom-40 -z-10 transform-gpu overflow-hidden blur-3xl'>
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className='relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem]  rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] md:opacity-30 opacity-40 sm:right-[calc(50%-30rem)] sm:w-[72.1875rem]'
+            />
+          </div>
+      </div>
+    </div>
+    </>
+  );
+}
---
+++ src/component/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import {cn} from '@/lib/utils'
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+    classname,
+    children
+}:{
+    classname?:string
+    children:ReactNode
+}) => {
+    return (
+        <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20 bg-yellow-400',classname!)}>
+            {children}
+        </div>
+    );
+}
+
+export default MaxWidthWrapper;
