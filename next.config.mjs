ee5858510c5914350aa35091bac459da237c8a16
ajinkya singh <azinc28@gmail.com>
2024-04-21 01:20:18 +0530

    *Dev* Payment

--- next.config.mjs
+++ next.config.mjs
@@ -8,20 +8,7 @@ const nextConfig = {
         config.resolve.alias.encoding = false
         return config
       },
-      async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "https://asher-ecru.vercel.app/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+     
       
 
       
